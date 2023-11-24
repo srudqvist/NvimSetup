25a1cb54afeedd4772d3610a85bc4e7b61b8921a
Sam <60146956+srudqvist@users.noreply.github.com>
2023-11-24 17:04:04 -0600

    Fixed flutter issues

--- lua/samuelrudqvist/plugins/flutter.lua
+++ lua/samuelrudqvist/plugins/flutter.lua
@@ -3,4 +3,13 @@ if not status then
     return
 end
 
-flutter_tools.setup()
+flutter_tools.setup({
+    widget_guides = {
+        enabled = false,
+    },
+    closing_tags = {
+        highlight = "ErrorMsg", -- highlight for the closing tag
+        prefix = ">", -- character to use for close tag e.g. > Widget
+        enabled = true -- set to false to disable
+    },
+})
