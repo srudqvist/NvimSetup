d167b643919653ea452f0dca74b62d843f0000a1
Sam <60146956+srudqvist@users.noreply.github.com>
2023-11-26 17:31:51 -0600

    flutter widget updates

--- lua/samuelrudqvist/plugins/flutter.lua
+++ lua/samuelrudqvist/plugins/flutter.lua
@@ -1,15 +1,15 @@
 local status, flutter_tools = pcall(require, "flutter-tools")
 if not status then
-    return
+	return
 end
 
 flutter_tools.setup({
-    widget_guides = {
-        enabled = false,
-    },
-    closing_tags = {
-        highlight = "ErrorMsg", -- highlight for the closing tag
-        prefix = ">", -- character to use for close tag e.g. > Widget
-        enabled = true -- set to false to disable
-    },
+	widget_guides = {
+		enabled = false,
+	},
+	closing_tags = {
+		highlight = "ErrorMsg", -- highlight for the closing tag
+		prefix = "//", -- character to use for close tag e.g. > Widget
+		enabled = true, -- set to false to disable
+	},
 })
