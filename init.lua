90da248f8f45d14e073f8d7bc879ce7b78ab7e5c
Sam <60146956+srudqvist@users.noreply.github.com>
2023-12-09 18:14:46 -0600

    Added highlight for yanking

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- Lua initialization file
 vim.g.nightflyTransparent = true
+
 require("samuelrudqvist.plugins-setup")
 require("samuelrudqvist.core.options")
 require("samuelrudqvist.core.keymaps")
@@ -18,5 +19,11 @@ require("samuelrudqvist.plugins.autopairs")
 require("samuelrudqvist.plugins.treesitter")
 require("samuelrudqvist.plugins.gitsigns")
 
+vim.cmd([[
+  augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=300})
+  augroup END
+]])
 -- Set tabwidth and shiftwidth to 2 for Dart files
 -- vim.api.nvim_command("autocmd FileType dart setlocal tabstop=2 shiftwidth=2")
